3630da1c0780ce56d0fe87bc811bf17dec36e785
Kevin Tena <kevten09@gmail.com>
2019-02-22 12:50:51 -0600

    Added Swagger documentation and config, db configs, and actuator.

--- src/main/java/com/lambdaschool/javatodos/TodoController.java
+++ src/main/java/com/lambdaschool/javatodos/TodoController.java
@@ -15,6 +15,7 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
+@API(value = "Todo Application", description = "Lambda School JX: Sprint Challenge 3")
 @RestController
 @RequestMapping(path = {}, produces = MediaType.APPLICATION_JSON_VALUE)
 public class TodoController {
@@ -25,11 +26,21 @@ public class TodoController {
     @Autowired
     UserRepository userrepos;
 
+    @ApiOperation("list ALL Todo", response = List.class)
+    @ApiResponses(value =
+            {
+                    @ApiResponse(code = 200, message = "Successfully  retrieved list"),
+                    @ApiResponse(code = 401, message = "You are not authorized to the view the resource"),
+                    @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+                    @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            })
+
     @GetMapping("/user")
     public List<User> alluser() {
         return userrepos.findAll();
     }
 
+    @ApiOperation(value = "Returns all the Todos in the database", response = Todo.class)
     @GetMapping("/todos")
     public List<Todo> alltodos() {
         return todorepos.findAll();
@@ -68,11 +79,13 @@ public class TodoController {
         }
     }
 
+    @ApiOperation(value ="Returns all todos and their users", response = Todo.class)
     @GetMapping("/todos/users")
     public List<Object[]> todoUsers(){
         return todorepos.todoUsers();
     }
 
+    @ApiOperation(value = "Returns all todos that are currently active", response = Todo.class)
     @GetMapping("/todos/active")
     public List<Todo> activeTodos(){
         return todorepos.findByCompleted(1);
@@ -105,6 +118,7 @@ public class TodoController {
         }
     }
 
+    @ApiOperation(value = "Updated a current Todo by using its ID or create a new one if it does not exist.", response = Todo.class)
     @PutMapping("/todos/todoid/{todoid}")
     public Todo changeTodo(@RequestBody Todo newtodo, @PathVariable int id){
 
@@ -134,6 +148,7 @@ public class TodoController {
             return null;
     }
 
+    @ApiOperation(value = "Deletes a Todo by ID and returns the deleted Todo or else Null", response = Todo.class)
     @DeleteMapping("/todos/todoid/{todoid}")
     public Todo deleteTodo(@PathVariable int id){
         var foundTodo = todorepos.findById(id);
